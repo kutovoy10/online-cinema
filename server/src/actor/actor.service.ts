42bf9712f522e58f04c2c91ac609df70081bf2f6
kutovoy10 <danil7kutovoy@gmail.com>
2025-06-22 16:09:57 +0200

    refactor actor service to use aggregation for movie count

--- server/src/actor/actor.service.ts
+++ server/src/actor/actor.service.ts
@@ -35,11 +35,17 @@ export class ActorService {
       }
     }
 
-    // Aggregation
-
-    return this.ActorModel.find(options)
-      .select('-updatedAt -__v')
-      .sort({ createdAt: 'desc' })
+    return this.ActorModel.aggregate()
+      .match(options)
+      .lookup({
+        from: 'movies',
+        localField: '_id',
+        foreignField: 'actors',
+        as: 'movies',
+      })
+      .addFields({ countMovies: { $size: '$movies' } })
+      .project({ __v: 0, updatedAt: 0, movies: 0 })
+      .sort({ createdAt: -1 })
       .exec()
   }
 
